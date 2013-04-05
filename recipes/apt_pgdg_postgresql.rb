ebefc06610e6887f55ff3183d06479e9811eaf49
Harrison Conlin <me@harrisony.com>
2013-04-05 14:17:11 +1100

    PGDG recipe now includes fallback to the pitti repository

--- recipes/apt_pgdg_postgresql.rb
+++ recipes/apt_pgdg_postgresql.rb
@@ -1,13 +1,23 @@
-if not %w(etch lenny lucid precise sid squeeze wheezy).include? node['postgresql']['pgdg']['release_apt_codename']
-  raise "Not supported release by PGDG apt repository"
-end
-
 include_recipe 'apt'
 
-apt_repository 'apt.postgresql.org' do
-  uri 'http://apt.postgresql.org/pub/repos/apt'
-  distribution "#{node['postgresql']['pgdg']['release_apt_codename']}-pgdg"
-  components %w(main)
-  key 'http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc'
-  action :add
+case node['postgresql']['pgdg']['release_apt_codename']
+when "etch", "lenny", "lucid", "precise", "sid", "squeeze", "wheezy"
+  apt_repository 'apt.postgresql.org' do
+    uri 'http://apt.postgresql.org/pub/repos/apt'
+    distribution "#{node['postgresql']['pgdg']['release_apt_codename']}-pgdg"
+    components %w(main)
+    key 'http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc'
+    action :add
+  end
+when "quantal", "oneiric"
+  apt_repository 'pitti-postgresql-ppa' do
+    uri 'http://ppa.launchpad.net/pitti/postgresql/ubuntu'
+    distribution node['lsb']['codename']
+    components %w(main)
+    keyserver 'keyserver.ubuntu.com'
+    key '8683D8A2'
+    action :add
+  end
+else
+  raise "Not supported release by PGDG apt repository"
 end
